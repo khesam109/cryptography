55c576a83cd524382176c3ca31cefdd029364d2c
M_Kalantari <M_Kalantari@isc.co.ir>
2024-08-14 09:43:28 +0330

    some refactor

--- papyrus/papyrus-core/src/main/resources/init-data.sql
+++ papyrus/papyrus-core/src/main/resources/init-data.sql
@@ -1,3 +1,3 @@
 
 INSERT INTO PAPYRUS.SIGNER(ID, FULL_NAME, WET_SIGNATURE_PATH, CERTIFICATE_PATH)
-    VALUES (1, 'Asghar Davaloo', 'D:\Personal\project\github\cryptography\papyrus\wet-signature\1-wet.png', 'D:\Personal\project\github\cryptography\papyrus\keystore\ks');
+    VALUES (1, 'Asghar Davaloo', 'wet-signature\1-wet.png', 'keystore\ks');
